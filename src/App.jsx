36619c96afe5719c5cfd20dd25c2b0eb3c17819d
B.K.S.S.S MALLIK <sssmallik143@gmail.com>
2025-08-08 18:34:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Vite + TailwindCSS Setup!!
+      </h1>
+    </div>
+  );
+}
