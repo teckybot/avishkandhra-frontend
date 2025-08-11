ed7c71046f0099aa798d03be950b09cdb0907507
B.K.S.S.S MALLIK <sssmallik143@gmail.com>
2025-08-11 17:47:33 +0530

    formpage added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,15 +2,18 @@ import { useRef } from 'react';
 import Logos from '../Components/Logos';
 import AvishkAndhraBanner from '../Components/Hero';
 import About from '../Components/About';
+import FormPage from '../components/FormPage';
 
 const Home = () => {
   const videoPageRef = useRef(null);
 
   return (
     <>
-    <Logos />
-    <AvishkAndhraBanner />
-    <About />
+      <Logos />
+      <AvishkAndhraBanner />
+      <About />
+      <FormPage />
+
     </>
   );
 };
